d179f299bcf6b77ccc6fc2114daef3df40a79cb9
Calisthetic <vovakalash04@mail.ru>
2024-03-04 17:00:29 +0500

    Add project files.

---
+++ PaintOnlineAPI/Controllers/FiguresController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using PaintOnlineAPI.Hubs;
+
+namespace PaintOnlineAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FiguresController : ControllerBase
+    {
+        private IHubContext<PaintHub, IPaintHubClient> paintHub;
+        public FiguresController(IHubContext<PaintHub, IPaintHubClient> _paintHub)
+        {
+            paintHub = _paintHub;
+        }
+        [HttpPost]
+        [Route("productoffers")]
+        public string Get()
+        {
+            List<string> offers = new List<string>();
+            offers.Add("Draw cube");
+            paintHub.Clients.All.SendOffersToUser(offers);
+            return "Offers sent successfully to all users!";
+        }
+    }
+}
---
+++ PaintOnlineAPI/Hubs/PaintHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PaintOnlineAPI.Hubs
+{
+    public interface IPaintHubClient
+    {
+        Task SendOffersToUser(List<string> message);
+    }
+    public class PaintHub : Hub<IPaintHubClient>
+    {
+        public async Task SendOffersToUser(List<string> message)
+        {
+            await Clients.All.SendOffersToUser(message);
+            //Groups
+            //Context.Abort();
+        }
+    }
+}
